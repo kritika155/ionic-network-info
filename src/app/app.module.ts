6d5c09341dea681dbae8ba42f6a5d6826f83d899
unknown <roykritika00@gmail.com>
2019-05-08 17:17:30 +0530

    network-info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { Network } from '@ionic-native/network/ngx';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Network } from '@ionic-native/network/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,24 +6,5 @@ import { Network } from '@ionic-native/network/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-constructor(private network: Network) { }
-networkInfo={
-  status:''
-}
-disconnectSubscription = this.network.onDisconnect().subscribe(() => {
-  console.log('network was disconnected :-(');
-  this.networkInfo.status='Disconnected'
 
-});
-connectSubscription = this.network.onConnect().subscribe(() => {
-  console.log('network connected!');
-  this.networkInfo.status='Connected';
-  //localStorage.setItem('NetworkStatus', JSON.stringify(this.networkInfo.status));
-
-  setTimeout(() => {
-    if (this.network.type === 'wifi') {
-      console.log('we got a wifi connection, woohoo!');
-    }
-  }, 3000);
-});
 }
